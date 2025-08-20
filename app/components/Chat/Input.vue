d0c1e6c88d59bd75cfcd5988e0b924941afd2229
Jet-Green <grishadzyin@gmail.com>
2025-08-20 10:58:17 +0400

    add emit handling

--- app/components/Chat/Input.vue
+++ app/components/Chat/Input.vue
@@ -1,24 +1,18 @@
 <script setup lang="ts">
+const emit = defineEmits(["submit"])
+
 const question = ref<string>("");
+
+function submit() {
+  emit("submit", question.value)
+}
 </script>
 <template>
-  <v-form class="w-100">
+  <v-form class="w-100" @submit.prevent="submit">
     <div class="d-flex align-center justify-center w-100">
-      <v-text-field
-        class="flex-grow-1 mr-2"
-        placeholder="Вопрос"
-        v-model="question"
-        variant="outlined"
-        hide-details
-      />
-      <v-btn
-        class="flex-shrink-0"
-        @click="$emit('Submit', question.valueOf())"
-        height="60"
-        width="40"
-      >
-        <v-icon size="40"> mdi-arrow-up-circle-outline </v-icon></v-btn
-      >
+      <v-text-field class="flex-grow-1 mr-2" placeholder="Вопрос" v-model="question" variant="outlined" hide-details />
+      <v-btn class="flex-shrink-0" height="60" width="40" type="submit">
+        <v-icon size="40"> mdi-arrow-up-circle-outline </v-icon></v-btn>
     </div>
   </v-form>
 </template>
--- app/pages/chat.vue
+++ app/pages/chat.vue
@@ -2,12 +2,16 @@
 definePageMeta({
   layout: "chat-layout"
 })
+
+function processSubmit(question: string) {
+  console.log(question);
+}
 </script>
 <template>
   <v-container>
     <v-row class="d-flex justify-center">
       <v-col cols="12" md="8" xl="6" class="chat-container">
-        <ChatInput />
+        <ChatInput @submit="processSubmit" />
       </v-col>
     </v-row>
   </v-container>
