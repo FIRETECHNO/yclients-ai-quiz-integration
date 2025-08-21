97c9a03ba512d980402f5a8db672be688c8f62a6
Poruch <139491864+Poruch@users.noreply.github.com>
2025-08-21 22:46:28 +0500

    add sending messages to server

---
+++ app/api/ChatApi.ts
@@ -0,0 +1,11 @@
+export default {
+  serverSendMessage(message: IMessage): Promise<{ success: boolean }> {
+    const { $apiFetch } = useNuxtApp();
+    return $apiFetch<{ success: boolean }>("/teacher/update-teacher-summary", {
+      method: "POST",
+      body: { message },
+    });
+  },
+
+  // другие функции через запятую
+};
--- app/composables/useChat.ts
+++ app/composables/useChat.ts
@@ -1,10 +1,20 @@
+import { useUser } from "./useUser";
+import ChatApi from "~/api/ChatApi";
 export function useChat() {
-  let messages = useState<IMessage[]>(() => [])
-  let chatStatus = useState<"ready" | "ai-thinking">("ready")
+  let messages = useState<IMessage[]>(() => []);
+  let chatStatus = useState<"ready" | "ai-thinking">("ready");
 
   async function sendMessage(question: string) {
     const { user } = useUser();
-    let messageOnClient = new Message(question, {}, false, user.value?.id)
+    let messageOnClient = new Message(question, {}, false, user.value?.id);
+
+    try {
+      const success = await ChatApi.serverSendMessage(messageOnClient);
+      return success.success;
+    } catch (error) {
+      console.error("Failed to send message to the server", error);
+      return false;
+    }
 
     messages.value.push(messageOnClient); // insert a new message
     chatStatus.value = "ai-thinking";
@@ -12,8 +22,9 @@ export function useChat() {
 
   return {
     // variables
-    messages, chatStatus,
+    messages,
+    chatStatus,
     // functions
     sendMessage,
-  }
+  };
 }
