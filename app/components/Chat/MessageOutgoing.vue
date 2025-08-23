9f50b1ed0082e8a47c466e03c1057b1c6990b239
Poruch <139491864+Poruch@users.noreply.github.com>
2025-08-23 22:43:59 +0500

    implemented the component MessageOutgoing and fix useChat

--- app/components/Chat/MessageOutgoing.vue
+++ app/components/Chat/MessageOutgoing.vue
@@ -1,8 +1,24 @@
 <script setup lang="ts">
-defineProps<{ message: IMessage }>()
+defineProps<{ message: IMessage }>();
 </script>
+
 <template>
-  <div>
-    {{ message }}
+  <div class="d-flex justify-md-end">
+    <v-card class="text-card">
+      <v-card-text>
+        <slot>{{ message.stringContent }}</slot>
+      </v-card-text>
+    </v-card>
   </div>
 </template>
+
+<style>
+.text-card {
+  max-width: 75%;
+  max-height: 600px;
+  width: fit-content;
+  height: fit-content;
+  overflow: auto;
+  text-align: left;
+}
+</style>
