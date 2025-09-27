e37bb2cba7dabc2b5f97aece1964b63d17cc26fd
Poruch <139491864+Poruch@users.noreply.github.com>
2025-09-27 21:37:04 +0500

    change style display service

--- app/components/Chat/MessageIncoming.vue
+++ app/components/Chat/MessageIncoming.vue
@@ -1,30 +1,32 @@
 <script setup lang="ts">
-import { Service } from '~/utils/service';
+import { Service } from "~/utils/service";
 
 defineProps<{ message: IMessage }>();
 const chatStore = useChat();
-const focusService = ref<IService>(new Service("Стрижка",
-      123,
-      false,
-      2323,
-      "Strishka",
-      324324,
-      0,
-      2000,
-      0,
-      "element.comment",
-      "false",
-      false,
-      [],
-      3600,
-      "https://assets.yclients.com/main_service_image/basic/5/56/568e4d6379dcf69_20240723100743.png"));
+const focusService = ref<IService>(
+  new Service(
+    "Стрижка",
+    123,
+    false,
+    2323,
+    "Strishka",
+    324324,
+    0,
+    2000,
+    0,
+    "element.comment",
+    "false",
+    false,
+    [],
+    3600,
+    "https://assets.yclients.com/main_service_image/basic/5/56/568e4d6379dcf69_20240723100743.png"
+  )
+);
 function onHintClick(message: IMessage, index: number) {
   console.log(message.payload.recommended_services[index]);
   //focusService.value = message.payload.recommended_services[index];
 }
-function onFocusServiceClick(){
-  
-}
+function onFocusServiceClick() {}
 </script>
 
 <template>
@@ -48,11 +50,29 @@ function onFocusServiceClick(){
           {{ service.serviceName }}
         </v-chip>
       </v-card>
-      <v-card v-if = "focusService != undefined"class="focus-service" @click="onFocusServiceClick()" color = "gray"> 
-        <H1>Процедура: {{ focusService.serviceName}}</H1>
-        <h2>Описание: {{  focusService.comment}}</h2>
-        <h3>Длительность: {{  focusService.duration / 60}} минут</h3>
-        <img :src="focusService.imagePath" alt = "service"></img>
+      <v-card
+        v-if="focusService"
+        class="focus-service"
+        @click="onFocusServiceClick()"
+        color="gray"
+        variant="outlined"
+      >
+        <v-card-title> Процедура: {{ focusService.serviceName }} </v-card-title>
+
+        <v-card-text>
+          <div><strong>Описание:</strong> {{ focusService.comment }}</div>
+          <div>
+            <strong>Длительность:</strong>
+            {{ Math.round(focusService.duration / 60) }} минут
+          </div>
+
+          <v-img
+            v-if="focusService.imagePath"
+            :src="focusService.imagePath"
+            alt="service"
+            cover
+          />
+        </v-card-text>
       </v-card>
     </v-card>
   </div>
@@ -66,7 +86,11 @@ function onFocusServiceClick(){
   overflow: auto;
   border-radius: 20px;
 }
-.focus-service{
+.focus-service {
   margin: 20px;
+  border: 3px solid #e51d1d;
+  border-radius: 12px;
+  transition: all 0.3s ease;
+  cursor: pointer;
 }
 </style>
